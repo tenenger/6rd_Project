73100913c2bfe89cadebd691675842f55ce4d7cc
tenenger <tenenger7125@gmail.com>
2022-06-29 23:30:02 +0900

    feat: add useRequest

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,1 +1,11 @@
-export const useRequest = () => {};
+import { useQuery } from "react-query";
+
+import { UserService } from "@src/services";
+
+const useRequest = () => {
+  return useQuery("me", UserService.me, {
+    // refetchInterval: 500,
+  });
+};
+
+export default useRequest;
