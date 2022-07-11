590733f846730b001c2fd250f4808fb7cb307e0b
tenenger <tenenger7125@gmail.com>
2022-07-12 00:11:27 +0900

    feat: add components index

---
+++ src/components/common/index.ts
@@ -0,0 +1,5 @@
+export { default as Button } from "./Button/Button";
+export { default as Select } from "./Select/Select";
+export { default as Layout } from "./Layout/Layout";
+export { default as Text } from "./Text/Text";
+export { default as FormInput } from "./FormInput/FormInput";
