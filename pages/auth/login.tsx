614d625c23f6e5e0b1520a614b011b734f9440db
tenenger <tenenger7125@gmail.com>
2022-07-12 00:15:58 +0900

    refactor: login page

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,41 +1,18 @@
 import Link from "next/link";
 
-import styled from "@emotion/styled";
-import { useForm } from "react-hook-form";
-import Button from "../../src/components/common/Button/Button";
-
-type UseForm = {
-  email: string;
-  password: string;
-};
+import { Button, Layout } from "@src/components/common";
+import LoginForm from "@src/components/UI/LoginForm/LoginForm";
 
 const LoginPage = () => {
-  // useForm hook의 type 설정 : useForm<UseForm>
-  const { register, handleSubmit } = useForm<UseForm>();
-
-  const onSubmit = () => {};
-
   return (
-    <Wrapper>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("email")} placeholder="아이디(이메일)" />
-        <input {...register("password")} placeholder="비밀번호" />
-        {/* type 속성을 주어, submit 기능 구현 */}
-        <Button type="submit">로그인</Button>
+    <Layout>
+      <LoginForm />
+      <Link href="/auth/signup">
         {/* a 태그 대신, Button 컴포넌트 사용 : forwardRef */}
-        <Link href="/auth/signup">
-          <Button>회원가입</Button>
-        </Link>
-      </form>
-    </Wrapper>
+        <Button>회원가입</Button>
+      </Link>
+    </Layout>
   );
 };
 
 export default LoginPage;
-
-// 로그인 페이지 목업 디자인
-const Wrapper = styled.div`
-  min-width: 290px;
-  max-width: 460px;
-  margin: auto;
-`;
