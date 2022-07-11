0b1aea7b4d6678bc2027218c82763c3147be7a3d
tenenger <tenenger7125@gmail.com>
2022-07-12 00:08:45 +0900

    feat: add Layout component

---
+++ src/components/common/Layout/Layout.style.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const SLayout = styled.div`
+  min-width: 290px;
+  max-width: 460px;
+  margin: auto;
+  line-height: 1.6;
+`;
---
+++ src/components/common/Layout/Layout.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { SLayout } from "./Layout.style";
+
+export default function Layout({ children }: any) {
+  return <SLayout>{children}</SLayout>;
+}
