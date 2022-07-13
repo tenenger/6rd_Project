d46e78686846f2ab8bc833248c177fc748641b72
tenenger <tenenger7125@gmail.com>
2022-07-13 17:34:49 +0900

    feat: add constants

---
+++ constants/formIcon.ts
@@ -0,0 +1,3 @@
+export { MdOutlineLocalPostOffice, MdOutlinePhoneIphone } from "react-icons/md";
+export { RiLockLine } from "react-icons/ri";
+export { BsPerson } from "react-icons/bs";
---
+++ constants/selectData.ts
@@ -0,0 +1,13 @@
+export const REQUIRE_DATA = [
+  { comment: "[필수] 만 14세 이상입니다." },
+  { comment: "[필수] 쿠팡 이용약관 동의" },
+  { comment: "[필수] 전자금융거래 이용약관 동의" },
+  { comment: "[필수] 개인정보 수집 및 이용 동의" },
+  { comment: "[필수] 개인정보 제 3자 제공 동의" },
+];
+export const OPTION_DATA = [
+  { comment: "[선택] 광고성 목적의 개인정보 수집 및 이용 동의" },
+  { comment: "[선택] 이메일 수신 동의" },
+  { comment: "[선택] SMS,MMS 수신 동의" },
+  { comment: "[선택] 앱 푸시 수신 동의" },
+];
